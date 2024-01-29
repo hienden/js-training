520178cc7448d5d58e901d60063f727445e71dd6
hienden <hoanghien110397@gmail.com>
2024-01-29 15:49:46 +0700

    add task 2

--- javascript/projects/project3/main.js
+++ javascript/projects/project3/main.js
@@ -3,12 +3,42 @@ const addTaskElement = document.getElementById("add-task");
 const taskListElement = document.querySelector(".task-list");
 
 addTaskElement.addEventListener("click", function () {
-  const li = document.createElement("li");
-  li.classList.add("row");
-  li.innerHTML = `<div><input type="checkbox" id="" /><span>${inputTaskElement.value}</span></div><button type="button" class="delete-task"><i class="fa-solid fa-x"></i></button>`;
-  taskListElement.appendChild(li);
-  inputTaskElement.value = "";
-  console.log(li);
-  console.log(taskListElement.innerHTML);
-  localStorage.setItem("data", taskListElement.innerHTML);
+  let todos = [];
+  const todo = {
+    id: Math.round(Math.random() * 10),
+    name: inputTaskElement.value,
+    completed: false,
+  };
+  console.log(todo);
+  todos.push(todo);
+  console.log(todos);
+
+  // let todos = [
+  //   {
+  //     id: 1,
+  //     name: "hien",
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "tho",
+  //     completed: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Anna",
+  //     completed: false,
+  //   },
+  // ];
+
+  const todosElement = todos.map(function (item) {
+    return `<li class="row"><div><input type="checkbox" onclick="handleCheck()"/><span>${item.name}</span></div><button type="button" class="delete-task" onclick="handleDelete('${item.id}')"><i class="fa-solid fa-x"></i></button></li>`;
+  });
+  console.log(todosElement);
+  taskListElement.innerHTML = todosElement.join("");
 });
+
+function handleDelete(id) {
+  console.log(id);
+}
+function handleCheck() {}
