7d70e38d8e36f895a51875365a5f952b669a1fa6
hienden <hoanghien110397@gmail.com>
2024-02-01 21:20:32 +0700

    done

--- javascript/projects/project3/main.js
+++ javascript/projects/project3/main.js
@@ -4,34 +4,43 @@ const taskListElement = document.querySelector(".task-list");
 
 let todos = [];
 
-function redenTodos(arrTodos) {
-  const todosArr = arrTodos.map(function (item) {
-    if (item.completed === true) {
-      const liElement = document.getElementById(item.id);
-      if (liElement) {
-        liElement.classList.add("checked");
-      }
-    }
-    return `<li class="row" id="${item.id}">
+// hàm hiển thị ra màn hình
+function redenTodos(todos) {
+  const todosList = todos.map(function (item) {
+    return `<li class="row ${item.completed ? "checked" : ""}" id="${item.id}">
               <div>
                 <input type="checkbox" onclick="handleChecked('${item.id}')"/>
                 <span>${item.name}</span>
               </div>
-              <button type="button" class="delete-task" onclick="handleDelete('${item.id}')">
+              <button type="button" class="delete-task" onclick="handleDelete('${
+                item.id
+              }')">
                 <i class="fa-solid fa-x"></i>
               </button>
             </li>`;
   });
-  taskListElement.innerHTML = todosArr.join("");
+  taskListElement.innerHTML = todosList.join("");
   inputTaskElement.value = "";
 }
 
-// Khi có dữ liệu => Lấy dữ liệu và in ra màn hình
-let reference = localStorage.getItem("data"); // kiểu JSON
-
-if (JSON.parse(reference)) {
-  redenTodos(JSON.parse(reference));
+// hàm lưu trữ dữ liệu vào localstorage
+function setLocalStorage(todos) {
+  localStorage.setItem("data", JSON.stringify(todos));
+  redenTodos(todos);
 }
+
+// hàm lấy dữ liệu từ localstorage
+function getLocalStorage() {
+  let reference = localStorage.getItem("data"); // kiểu JSON
+
+  if (reference) {
+    todos = JSON.parse(reference); // important
+    redenTodos(todos);
+  }
+}
+// Khi có dữ liệu => Lấy dữ liệu và in ra màn hình
+
+getLocalStorage();
 
 // Ấn vào Add => thêm công việc, lưu dữ liệu và in ra màn hình
 addTaskElement.addEventListener("click", function (e) {
@@ -40,28 +49,31 @@ addTaskElement.addEventListener("click", function (e) {
     alert("vui long nhap input");
     return;
   }
-  // if (JSON.parse(reference)) {
-  //   todos = todos.concat(JSON.parse(reference));
-  // }
+
   const todo = {
     id: Math.round(Math.random() * 10),
     name: inputTaskElement.value,
-    completed: true,
+    completed: false,
   };
   todos.push(todo);
-  localStorage.setItem("data", JSON.stringify(todos));
-  redenTodos(todos);
+  setLocalStorage(todos);
 });
 
 // xóa công việc, cập nhật lại dữ liệu và hiển thị
 function handleDelete(id) {
-  let reference = localStorage.getItem("data");
-  reference = JSON.parse(reference).filter(function (element) {
+  getLocalStorage();
+  const todo = todos.filter(function (element) {
     return element.id !== Number(id);
   });
-  localStorage.setItem("data", JSON.stringify(reference));
-  redenTodos(reference);
+  setLocalStorage(todo);
 }
 
 // chuyển đổi giá trị hoàn thành và chưa hoàn thành
-function handleChecked(id) {}
+function handleChecked(id) {
+  getLocalStorage();
+  const todo = todos.find(function (item) {
+    return item.id === Number(id);
+  });
+  todo.completed = !todo.completed;
+  setLocalStorage(todos);
+}
